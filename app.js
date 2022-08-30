9be0b320bc100efafd9b73046fd0f61288da2ba1
Muhally <muhammadalidehghanian@gmail.com>
2022-08-30 18:44:46 +0430

    JS-miniproject-Sidebar

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const toggleBtn = document.querySelector('.sidebar-toggle')
+const closeBtn = document.querySelector('.close-btn')
+const sideBar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click' , () => {
+
+    
+})
+
+
+
+
+
+
